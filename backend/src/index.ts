24064f3329163dcdc2f0e76a52e0a8e4d6afa826
HAMZAH ABDALQADER DARAWSHEH <hamzehdrawsheh04@gmail.com>
2026-05-27 00:59:26 +0300

    Delete backend directory

--- backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "./config";
-import { getDb } from "./db/connection";
-import app from "./app";
-
-getDb();
-
-app.listen(config.PORT, () => {
-  console.log(`Server listening on http://localhost:${config.PORT}`);
-});
